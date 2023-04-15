08723e9de0952362e942e8be066724186a8840b4
anurag-ay <ay1909069@gmail.com>
2023-04-15 16:20:15 +0530

    imporved css

--- Flask-Deployed-App/app.py
+++ Flask-Deployed-App/app.py
@@ -66,7 +66,9 @@ def submit():
 @app.route('/market', methods=['GET', 'POST'])
 def market():
     return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
-                           supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
+                                          supplement_name = list(supplement_info['supplement name']), 
+                                          disease = list(disease_info['disease_name']), 
+                                          buy = list(supplement_info['buy link']))
 
 if __name__ == '__main__':
     app.run(debug=True)
