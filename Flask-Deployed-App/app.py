f03757976b1f8496203e0738b4081af28ca8e0a9
anurag-ay <ay1909069@gmail.com>
2023-05-19 15:24:30 +0530

    edited contactUs page

--- Flask-Deployed-App/app.py
+++ Flask-Deployed-App/app.py
@@ -8,12 +8,13 @@ import torch
 import pandas as pd
 
 
-disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
-supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
+disease_info = pd.read_csv('disease_info.csv', encoding='cp1252')
+supplement_info = pd.read_csv('supplement_info.csv', encoding='cp1252')
 
-model = CNN.CNN(39)    
+model = CNN.CNN(39)
 model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
 model.eval()
+
 
 def prediction(image_path):
     image = Image.open(image_path)
@@ -28,21 +29,26 @@ def prediction(image_path):
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_page():
     return render_template('home.html')
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact-us.html')
 
+
 @app.route('/index')
 def ai_engine_page():
     return render_template('index.html')
 
+
 @app.route('/mobile-device')
 def mobile_device_detected_page():
     return render_template('mobile-device.html')
+
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
@@ -54,21 +60,24 @@ def submit():
         print(file_path)
         pred = prediction(file_path)
         title = disease_info['disease_name'][pred]
-        description =disease_info['description'][pred]
+        description = disease_info['description'][pred]
         prevent = disease_info['Possible Steps'][pred]
         image_url = disease_info['image_url'][pred]
         supplement_name = supplement_info['supplement name'][pred]
         supplement_image_url = supplement_info['supplement image'][pred]
         supplement_buy_link = supplement_info['buy link'][pred]
-        return render_template('submit.html' , title = title , desc = description , prevent = prevent , 
-                               image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
+        return render_template('submit.html', title=title, desc=description, prevent=prevent,
+                               image_url=image_url, pred=pred, sname=supplement_name, simage=supplement_image_url, buy_link=supplement_buy_link)
+
 
 @app.route('/market', methods=['GET', 'POST'])
 def market():
-    return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
-                                          supplement_name = list(supplement_info['supplement name']), 
-                                          disease = list(disease_info['disease_name']), 
-                                          buy = list(supplement_info['buy link']))
+    return render_template('market.html', supplement_image=list(supplement_info['supplement image']),
+                           supplement_name=list(
+                               supplement_info['supplement name']),
+                           disease=list(disease_info['disease_name']),
+                           buy=list(supplement_info['buy link']))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
